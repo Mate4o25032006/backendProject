6c7b366cbd110466070f039f93aaa4da3a435ec4
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-02-17 15:23:08 -0500

    Add: Cambio configuración seguridad.

--- src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
+++ src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package com.backend.backendProject.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "CookieAuth", // Cambia el nombre según tu preferencia
+        scheme = "cookie" // Indica que la autenticación se basa en cookies
+)
 @OpenAPIDefinition(
         info = @Info(
                 title = "API Módulo de Registro"
