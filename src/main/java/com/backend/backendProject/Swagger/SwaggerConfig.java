b8049c72006801e329544a8f8d94b7c43c71808d
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-02-17 16:45:38 -0500

    Add: Cambio configuración seguridad X4.

--- src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
+++ src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.backend.backendProject.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        // JWT configuration
+        type = SecuritySchemeType.HTTP,
+        name = "BearerAuth",
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @OpenAPIDefinition(
         info = @Info(
                 title = "API Módulo Registro"
@@ -19,7 +27,5 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 public class SwaggerConfig {
-    public OpenAPI api(){
-        return new OpenAPI();
-    }
+
 }
