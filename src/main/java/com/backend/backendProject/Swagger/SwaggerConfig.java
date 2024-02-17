7d8064e38b0a40e759095af321e13e8587415180
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-02-17 15:38:25 -0500

    Add: Cambio configuración seguridad X2.

--- src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
+++ src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
@@ -1,28 +1,13 @@
 package com.backend.backendProject.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        type = SecuritySchemeType.HTTP,
-        name = "CookieAuth", // Cambia el nombre según tu preferencia
-        scheme = "cookie" // Indica que la autenticación se basa en cookies
-)
-@OpenAPIDefinition(
-        info = @Info(
-                title = "API Módulo de Registro"
-                )
-        ,
-        servers = {
-                @Server(url="https://backendproject-production-6d25.up.railway.app/",description="Servidor de Producción"),
-                @Server(url = "http://localhost:8081/", description = "Servidor local")
-        }
-)
+@OpenAPIDefinition
 public class SwaggerConfig {
-
+    public OpenAPI api(){
+        return new OpenAPI();
+    }
 }
