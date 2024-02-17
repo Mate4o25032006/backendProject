7fa41f2a7715018bb2edd2ab44421cec87fe7d1e
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-02-17 14:56:47 -0500

    Add: Cambio configuración seguridad.

--- src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
+++ src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
@@ -8,16 +8,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        type = SecuritySchemeType.HTTP,
-        name = "BearerAuth",
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 @OpenAPIDefinition(
         info = @Info(
                 title = "API Módulo de Registro"
-                ,description = "Esta aplicación fue diseñada en el marco del proyecto integrador del")
+                )
         ,
         servers = {
                 @Server(url="https://backendproject-production-6d25.up.railway.app/",description="Servidor de Producción"),
