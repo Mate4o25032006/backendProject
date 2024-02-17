b0768bf46097e5d83beefac0a7240266d845f45b
Mateo Espinosa <mateoespinosa7777@gmail.com>
2024-02-17 15:47:16 -0500

    Add: Cambio configuración seguridad X3.

--- src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
+++ src/main/java/com/backend/backendProject/Swagger/SwaggerConfig.java
@@ -1,11 +1,23 @@
 package com.backend.backendProject.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Módulo Registro"
+                )
+
+        ,
+        servers = {
+                @Server(url="https://backendproject-production-6d25.up.railway.app//",description="Servidor de Producción"),
+                @Server(url = "http://localhost:8081/", description = "Servidor local")
+        }
+)
 public class SwaggerConfig {
     public OpenAPI api(){
         return new OpenAPI();
